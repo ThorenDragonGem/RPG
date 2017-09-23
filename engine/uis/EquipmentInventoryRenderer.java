ea413434f08d74e4e17d848fa3feddd164998d3c
ThorenDragonGem <thoren.dragon@gmail.com>
2017-09-23 20:47:36 +0200

    Starts EquipmentInvnetoryRenderer

--- engine/uis/EquipmentInventoryRenderer.java
+++ engine/uis/EquipmentInventoryRenderer.java
@@ -4,6 +4,43 @@ import java.awt.Graphics;
 
 public class EquipmentInventoryRenderer extends UI
 {
+	private int imageWidth = 64, imageHeight = 64;
+	// helm
+	private int helmX = 184, helmY = 16;
+	// chestplate
+	private int chestplateX = 184, chestplateY = 116;
+	// belt
+	private int beltX = 184, beltY = 209;
+	// greaves
+	private int greavesX = 184, greavesY = 290;
+	// boot1
+	private int boot1X = 441, boot1Y = 373;
+	// boot2
+	private int boot2X = 228, boot2Y = 373;
+	// talisman1
+	private int talisman1X = 52, talisman1Y = 27;
+	// talisman2
+	private int talisman2X = 316, talisman2Y = 27;
+	// shoulderpad1
+	private int shoulderPad1X = 92, shoulderPad1Y = 101;
+	// shoulderpad2
+	private int shoulderPad2X = 276, shoulderPad2Y = 101;
+	// armband1
+	private int armband1X = 92, armband1Y = 182;
+	// armband2
+	private int armband2X = 276, armband2Y = 182;
+	// gauntlets1
+	private int gauntlets1X = 100, gauntlets1Y = 263;
+	// gauntlets2
+	private int gauntlets2X = 268, gauntlets2Y = 263;
+	// weapon1
+	private int weapon1X = 19, weapon1Y = 344;
+	// weapon2
+	private int weapon2X = 349, weapon2Y = 344;
+	// mantle
+	private int mantleX = 354, mantleY = 168;
+	// parchment
+	private int parchmetnX = 14, parchmentY = 167;
 
 	public EquipmentInventoryRenderer()
 	{
